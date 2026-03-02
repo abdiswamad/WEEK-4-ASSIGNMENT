1104a41bb445e408f02749785f19bcf54b49394a
abdiswamad <abdisamadmohamud@students.zetech.ac.ke>
2026-03-02 20:21:05 +0300

    Add files via upload

---
+++ LOOP.C
@@ -0,0 +1,14 @@
+//program for loop printing from 100 down to 50
+//Name: ABDISAMAD ALI MOHAMUD
+//REG NO: BCS-03-0001/2026
+
+#include <stdio.h>
+
+int main() {
+	int i;
+	
+		for (i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
